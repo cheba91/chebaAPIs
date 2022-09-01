5c6f4466cd8036818abbbf76080a1e320fe3d15e
cheba91 <blaz.cebasek@gmail.com>
2022-09-01 18:03:28 +0200

    starting api dashboard

---
+++ components/MetaHead.js
@@ -0,0 +1,40 @@
+import Head from 'next/head';
+
+export default function MetaHead({
+   title = 'chebaAPIs',
+   keywords = "Frontend for my API's",
+   description = `Frontend for my API's`,
+   image = '/favicon.ico', // link to image of current page
+   //    ogType = 'blog', // type of current page
+}) {
+   const staticData = {
+      baseUrl: 'https://www.cheba.me',
+      companyName: 'Cheba - Personal website',
+      twitterHandle: '@BlazCheba',
+   };
+   return (
+      <Head>
+         {/* //basic metadata */}
+         <title>{title}</title>
+         <meta name="viewport" content="width=device-width, initial-scale=1" />
+         <meta name="description" content={description} />
+         <meta name="keywords" content={keywords} />
+         {/* //twitter metadata */}
+         <meta name="twitter:card" content="summary" />
+         <meta name="twitter:site" content={staticData.twitterHandle} />
+         <meta name="twitter:title" content={title} />
+         <meta name="twitter:description" content={description} />
+         <meta name="twitter:image" content={staticData.baseUrl + image} />
+         {/* //canonical link */}
+         <link rel="canonical" href={staticData.baseUrl} />
+         {/* //open graph metadata */}
+         <meta property="og:locale" content="en_US" />
+         <meta property="og:site_name" content={staticData.companyName} />
+         <meta property="og:type" content={ogType} />
+         <meta property="og:title" content={title} />
+         <meta property="og:description" content={description} />
+         <meta property="og:image" content={staticData.baseUrl + image} />
+         <meta property="og:url" content={staticData.baseUrl} />
+      </Head>
+   );
+}
---
+++ components/SocialIcons.js
@@ -0,0 +1,35 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Grid, Link, useTheme } from '@mui/material';
+
+const IconLink = ({ link, children }) => {
+   const textClr = useTheme().palette.text.primary;
+   return (
+      <Link sx={{ color: textClr }} href={link} rel="noopener" target="_blank">
+         {children}
+      </Link>
+   );
+};
+
+export default function SocialIcons({ customStyles }) {
+   return (
+      <Grid
+         sx={{ ...customStyles }}
+         container
+         justifyContent="center"
+         alignItems="center"
+         gap={1}
+      >
+         <Grid item>
+            <IconLink link={'https://github.com/cheba91'}>
+               <GitHubIcon />
+            </IconLink>
+         </Grid>
+         <Grid item>
+            <IconLink link={'https://www.linkedin.com/in/blazcebasek/'}>
+               <LinkedInIcon />
+            </IconLink>
+         </Grid>
+      </Grid>
+   );
+}
